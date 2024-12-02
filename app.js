2ee5a391a562dbff1306c389b8113f46abeb6aea
XxCatrinxX <108961916+XxCatrinxX@users.noreply.github.com>
2024-12-01 23:34:28 -0600

    Update app.js

--- app.js
+++ app.js
@@ -74,6 +74,10 @@ app.use((err, req, res, next) => {
   res.status(err.status || 500).json({ error: err.message });
 });
 
-
+// Crear servidor HTTP
+const PORT = process.env.PORT || 3000; // Railway asignará automáticamente un puerto
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
 
 module.exports = app;
